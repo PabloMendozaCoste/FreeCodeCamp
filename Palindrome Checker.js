adf47c12aeb5ebdf877ee1083e337c46b6e1d8a0
PabloMendozaCoste <49158000+PabloMendozaCoste@users.noreply.github.com>
2022-11-16 02:32:33 -0400

    Update Palindrome Checker.js

--- Palindrome Checker.js
+++ Palindrome Checker.js
@@ -7,6 +7,5 @@ function palindrome(str) {
     }
 }
 
-/*Return true if the given string is a palindrome. Otherwise, return false.
-
-A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.*\
+/*Return true if the given string is a palindrome. Otherwise, return false.*\
+/*A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.*\
